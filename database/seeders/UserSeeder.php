c49a10ab6b026eaba16fcb42a0f4e0a44028f5ca
mas-yan <mrey5597@gmail.com>
2022-03-03 14:55:18 +0700

    add role user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('password')
+        ]);
+
+        $admin->assignRole('admin');
+
+        $user = User::create([
+            'name' => 'penulis',
+            'email' => 'penulis@gmail.com',
+            'password' => bcrypt('password')
+        ]);
+
+        $user->assignRole('penulis');
+    }
+}
