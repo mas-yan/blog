db901ed31a910a92dc2a4630a4bd44e5e71fe7a4
mas-yan <mrey5597@gmail.com>
2022-03-11 21:23:23 +0700

    sidebar

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -73,14 +73,6 @@
           </a>
         </li>
       @endcan
-      <li class="nav-item">
-        <a class="nav-link {{ request()->is('categories') || request()->is('categories/*') ? 'active' : '' }}" href="{{ route('categories.index') }}">
-          <div class="icon-sm shadow border-radius-md bg-dark text-white text-center me-2 d-flex align-items-center justify-content-center">
-            <i class="fa fa-solid fa-trash"></i>
-          </div>
-          <span class="nav-link-text ms-1">trash</span>
-        </a>
-      </li>
       <li class="nav-item mt-3">
         <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Account pages</h6>
       </li>
