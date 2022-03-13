a50773c87b5b292678fe9ddba5ccb5da7cd405fa
mas-yan <mrey5597@gmail.com>
2022-03-13 19:31:46 +0700

    edit footer sidebar

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -29,8 +29,6 @@
   <!-- Nucleo Icons -->
   <link href="{{ asset('') }}assets/css/nucleo-icons.css" rel="stylesheet" />
   <link href="{{ asset('') }}assets/css/nucleo-svg.css" rel="stylesheet" />
-  <!-- Font Awesome Icons -->
-  {{-- <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script> --}}
   <script src="https://kit.fontawesome.com/dbf5dfd73c.js" crossorigin="anonymous"></script>
   <link href="{{ asset('') }}assets/css/nucleo-svg.css" rel="stylesheet" />
   <!-- CSS Files -->
@@ -61,29 +59,12 @@
         <div class="row align-items-center justify-content-lg-between">
           <div class="col-lg-6 mb-lg-0 mb-4">
             <div class="copyright text-center text-sm text-muted text-lg-start">
-              © <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script>
-                document.write(new Date().getFullYear())
-              </script>,
+              © 2022 - <script>
+                  document.write(new Date().getFullYear())
+                </script>,
               made with <i class="fa fa-heart"></i> by
-              <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Creative Tim</a>
-              for a better web.
+              <a href="https://github.com/mas-yan" class="font-weight-bold" target="_blank">Riyan Alfian</a>
             </div>
-          </div>
-          <div class="col-lg-6">
-            <ul class="nav nav-footer justify-content-center justify-content-lg-end">
-              <li class="nav-item">
-                <a href="https://www.creative-tim.com" class="nav-link text-muted" target="_blank">Creative Tim</a>
-              </li>
-              <li class="nav-item">
-                <a href="https://www.creative-tim.com/presentation" class="nav-link text-muted" target="_blank">About Us</a>
-              </li>
-              <li class="nav-item">
-                <a href="https://creative-tim.com/blog" class="nav-link text-muted" target="_blank">Blog</a>
-              </li>
-              <li class="nav-item">
-                <a href="https://www.creative-tim.com/license" class="nav-link pe-0 text-muted" target="_blank">License</a>
-              </li>
-            </ul>
           </div>
         </div>
       </div>
@@ -105,8 +86,6 @@
       Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
     }
   </script>
-  <!-- Github buttons -->
-  <script async defer src="https://buttons.github.io/buttons.js"></script>
   <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
   <script src="{{ asset('') }}assets/js/soft-ui-dashboard.min.js?v=1.0.3"></script>
   <script>
